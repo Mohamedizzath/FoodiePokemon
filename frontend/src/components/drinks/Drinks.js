f29e40057982ab46763db6a5573804d6a70b259c
Mohamedizzath <41626585+Mohamedizzath@users.noreply.github.com>
2022-09-19 21:51:47 +0530

    Added initial pages and react-router (#1)

---
+++ frontend/src/components/drinks/Drinks.js
@@ -0,0 +1,9 @@
+import 'bootswatch/dist/journal/bootstrap.css';
+
+function Drinks(){
+    return <div className="container">
+        <h1>Drinks Page</h1>
+    </div>
+}
+
+export default Drinks;
---
+++ frontend/src/components/foodItems/FoodItems.js
@@ -0,0 +1,9 @@
+import 'bootswatch/dist/journal/bootstrap.css';
+
+function FoodItems(){
+    return <div className="container">
+        <h1>FoodItems Page</h1>
+    </div>
+}
+
+export default  FoodItems;
