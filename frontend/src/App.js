9af1c6a258714835b02aefd4d4a177fb4ce47707
Gitash4cs <84612326+Gitash4cs@users.noreply.github.com>
2022-10-25 08:09:36 +0530

    coded feedback.js (#14)
    Co-authored-by: Mohamedizzath <41626585+Mohamedizzath@users.noreply.github.com>

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,6 +3,7 @@ import {BrowserRouter as Router, Link, Route, Routes, useNavigate} from "react-r
 import Drinks from "./components/drinks/Drinks";
 import Orders from "./components/orders/Orders";
 import Payments from "./components/payments/Payments";
+import Feedback from "./pages/Feedback";
 import HomePage from "./HomePage";
 import LoginPage from "./pages/LoginPage";
 import {useState} from "react";
@@ -41,6 +42,9 @@ function App() {
                             <li className="nav-item">
                                 <Link to="/payments" className="nav-link">Payments</Link>
                             </li>
+                            <li className="nav-item">
+                                <Link to="/feedback" className="nav-link">Feedback</Link>
+                            </li>
                         </ul>
                         <div className="d-flex">
                             {user && <Link to="/login" className="btn btn-danger my-2 my-sm-0"
@@ -58,6 +62,8 @@ function App() {
                 <Route path='/orders' element={<Orders/>}/>
                 <Route path='/payments' element={<Payments/>}/>
                 <Route path='/login' element={<LoginPage setUser={setUser}/>}/>
+                <Route path='/feedback' element={<Feedback/>}/>
+
             </Routes>
             <div className="container-fluid py-2 mt-0 text-center bg-dark">
                 <a href="https://github.com/Mohamedizzath/FoodiePokemon" className="text-decoration-none"
