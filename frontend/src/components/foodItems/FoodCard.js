236340b046f4e2fadef774821011cf3924712dae
Mohamedizzath <mohamedizzath2016@gmail.com>
2022-10-26 18:12:03 +0530

    Added cart add/remove to the foodcard

--- frontend/src/components/foodItems/FoodCard.js
+++ frontend/src/components/foodItems/FoodCard.js
@@ -1,6 +1,17 @@
 import {FaTrash, FaPen} from "react-icons/fa";
 
-function FoodCard({foodItem, showUpdate, showDelete, deleteItem, setFoodItemId}) {
+function FoodCard({foodItem, showUpdate, showDelete, deleteItem, setFoodItemId, cart, setCart}) {
+    const addOrRemoveItem = (foodItem) => {
+        if(cart.foodItems.includes(foodItem)){
+            const newFoodItems = cart.foodItems.filter(item => item._id !== foodItem._id);
+            setCart({...cart, foodItems: [...newFoodItems]});
+        } else {
+            const newCart = {...cart, foodItems: [...cart.foodItems, foodItem]};
+            setCart(newCart);
+        }
+        console.log(cart);
+    }
+
     return <div className="card bg-light mb-3 mx-4" style={{minWidth: 250, maxWidth: 300}} id={foodItem._id}>
         <div className="card-header d-flex justify-content-between">
             <h3>{foodItem.Name}</h3>
@@ -16,7 +27,7 @@ function FoodCard({foodItem, showUpdate, showDelete, deleteItem, setFoodItemId})
         </div>
         <div className="container text-center py-4 mt-auto">
             <button type="button" className="btn btn-outline-success">Rs. {foodItem.Price}</button>
-            <button type="button" className="btn btn-danger ms-4">Order now!</button>
+            <button type="button" disabled={!cart.user}  className="btn btn-danger ms-4" onClick={() => addOrRemoveItem(foodItem)}>{ cart.foodItems.includes(foodItem) ? "Remove" : "Order Now!" }</button>
             {showDelete &&
                 <button type="button" className="btn btn-danger ms-2 px-2" onClick={() => deleteItem(foodItem._id)}>
                     <FaTrash/>
