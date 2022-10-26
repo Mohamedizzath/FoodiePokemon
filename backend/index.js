adfab091dc7435d871af47e71954339fd140dc51
Mohamedizzath <mohamedizzath2016@gmail.com>
2022-10-26 18:00:06 +0530

    Added routes for index.js

--- backend/index.js
+++ backend/index.js
@@ -3,6 +3,10 @@ const app = express();
 const port = 8080;
 
 const FoodItemsRoutes = require('./routes/foodItems');
+const DrinksRoutes = require('./routes/drinks');
+const DessertRoutes = require('./routes/desserts');
+const FeedbackRoutes = require('./routes/feedbacks');
+const AuthRoutes = require('../../FoodiePokemon/backend/routes/auth');
 
 // Reading environmental variables
 const dotenv = require('dotenv');
@@ -15,17 +19,19 @@ app.use(cors());
 const bodyparser = require('body-parser');
 app.use(bodyparser.json());
 
-
 // Connecting to the mongodb
 const connectionString = process.env.CONNECTION_STRING;
 const startConnection = require('./mongo-config');
 startConnection(connectionString).then(() => {
     console.log('Connected to the mongodb server!');
 });
-const FoodItems = require('./models/fooditems');
 
 // Contains routes
 app.use('/fooditems', FoodItemsRoutes);
+app.use('/drinks', DrinksRoutes);
+app.use('/desserts', DessertRoutes);
+app.use('/feedbacks', FeedbackRoutes);
+app.use('/auth', AuthRoutes);
 
 app.listen(port, () => {
     console.log(`App listen on ${port}`);
