9c5dffbedd8dcf238c2c012d9284b78dffc4e7c0
Rushdha20 <89083702+Rushdha20@users.noreply.github.com>
2022-10-26 11:24:25 +0530

    Added CRUD operation for desserts to the frontend (#21)
    * Added PaymentsPage
    
    * dessert
    
    * dessert list
    
    * Add files via upload
    
    * Add files via upload
    
    * Added CRUD operation for desserts
    
    * Added CRUD operation for desserts to the frontend
    
    Co-authored-by: Mohamedizzath <41626585+Mohamedizzath@users.noreply.github.com>

--- frontend/src/components/desserts/DessertCard.js
+++ frontend/src/components/desserts/DessertCard.js
@@ -1,10 +1,20 @@
 import {FaTrash, FaPen} from "react-icons/fa";
 
-function DessertCard({DessertItem, showUpdate, showDelete, deleteItem}) {
+function DessertCard({DessertItem, showUpdate, showDelete, deleteItem, setDessertId, cart, setCart}) {
+    const addOrRemoveItem = (dessertItem) => {
+        if(cart.desserts.includes(dessertItem)){
+            const newDessertItems = cart.desserts.filter(item => item._id !== dessertItem._id);
+            setCart({...cart, desserts: [...newDessertItems]});
+        } else {
+            const newCart = {...cart, desserts: [...cart.desserts, dessertItem]};
+            setCart(newCart);
+        }
+        console.log(cart);
+    }
     return <div className="card bg-light mb-3 mx-4" style={{minWidth: 250, maxWidth: 300}} id={DessertItem._id}>
         <div className="card-header d-flex justify-content-between">
             <h3>{DessertItem.Name}</h3>
-            {showUpdate && <button type="button" className="btn btn-outline-success px-2">
+            {showUpdate && <button type="button" className="btn btn-outline-success px-2" onClick={() => setDessertId(DessertItem._id)}>
                 <FaPen/>
             </button>
             }
@@ -16,7 +26,7 @@ function DessertCard({DessertItem, showUpdate, showDelete, deleteItem}) {
         </div>
         <div className="container text-center py-4 mt-auto">
             <button type="button" className="btn btn-outline-success">Rs. {DessertItem.Price}</button>
-            <button type="button" className="btn btn-danger ms-4">Order now!</button>
+            <button type="button" disabled={!cart.user}  className="btn btn-danger ms-4" onClick={() => addOrRemoveItem(DessertItem)}>{ cart.desserts.includes(DessertItem) ? "Remove" : "Order Now!" }</button>
             {showDelete &&
                 <button type="button" className="btn btn-danger ms-2 px-2" onClick={() => deleteItem(DessertItem._id)}>
                     <FaTrash/>
